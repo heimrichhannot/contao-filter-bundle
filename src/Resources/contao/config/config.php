56d9fd7a72936a0eec81d99ad390a52af35738b8
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-01-18 16:11:16 +0100

    changed table names

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,7 +4,7 @@
  * Backend modules
  */
 $GLOBALS['BE_MOD']['system']['filter'] = [
-    'tables' => ['tl_filter', 'tl_filter_element'],
+    'tables' => ['tl_filter_config', 'tl_filter_config_element'],
     'option' => ['contao.controller.backend_csv_import', 'importOptionWizard']
 ];
 
