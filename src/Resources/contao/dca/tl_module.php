f4ee466f091823a82b4c5cb4747f16a8990a96b1
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-02-06 09:29:23 +0100

    added lazy loading listConfig within tl_module

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -16,7 +16,7 @@ $fields = [
         'exclude'    => true,
         'inputType'  => 'select',
         'foreignKey' => 'tl_filter_config.title',
-        'relation'   => ['type' => 'belongsTo', 'load' => 'eager'],
+        'relation'   => ['type' => 'belongsTo', 'load' => 'lazy'],
         'eval'       => ['tl_class' => 'w50 clr', 'includeBlankOption' => true, 'mandatory' => true],
         'sql'        => "int(10) NOT NULL default '0'",
     ],
