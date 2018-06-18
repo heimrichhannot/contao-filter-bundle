4cd8174b5187c7486c51ae5a0b7b4dbc654480f8
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-06-18 10:55:54 +0200

    renamed backend module name

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -5,7 +5,7 @@ $lang = &$GLOBALS['TL_LANG'];
 /**
  * Back end modules
  */
-$lang['MOD']['filter'] = ['Sortier-/Filterkonfigurationen', 'Sortier-/Filter-Konfigurationen verwalten'];
+$lang['MOD']['filter'] = ['Filter-/Sortierkonfigurationen', 'Filter-/Sortierkonfigurationen verwalten'];
 
 /**
  * Front end modules
