ec4ccac3e0bc3b3114769a0384ae2e02cb318771
Martin Kunitzsch <m.kunitzsch@heimrich-hannot.de>
2019-06-14 14:02:00 +0200

    created legacy js

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,16 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     .setOutputPath('src/Resources/public/js/')
-    .addEntry('contao-no-ui-slider-bundle', './src/Resources/assets/js/contao-no-ui-slider-bundle.js')
+    .addEntry('contao-filter-bundle', './src/Resources/assets/js/contao-filter-bundle.js')
     .setPublicPath('/public/js/')
     .disableSingleRuntimeChunk()
     .addExternals({
-        'nouislider': 'noUiSlider',
         '@hundh/contao-utils-bundle': 'utilsBundle'
     })
     .configureBabel(function (babelConfig) {
     }, {
         // include to babel processing
-        includeNodeModules: ['@hundh/contao-no-ui-slider-bundle']
+        includeNodeModules: ['@hundh/contao-filter-bundle']
     })
     .enableSourceMaps(!Encore.isProduction())
 ;
