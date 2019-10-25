2fd52edee259a88fad5340f4a2d6569ddacccc89
Thomas Körner <t.koerner@heimrich-hannot.de>
2019-10-25 13:19:11 +0200

    set contao environment request url to referrer in ajax calls

--- src/Controller/FrontendAjaxController.php
+++ src/Controller/FrontendAjaxController.php
@@ -8,11 +8,13 @@
 
 namespace HeimrichHannot\FilterBundle\Controller;
 
+use Contao\Environment;
 use Contao\FrontendIndex;
 use Contao\System;
 use HeimrichHannot\FilterBundle\Event\ModifyJsonResponseEvent;
 use HeimrichHannot\FilterBundle\Exception\HandleFormException;
 use HeimrichHannot\FilterBundle\Exception\MissingFilterException;
+use HeimrichHannot\FilterBundle\Form\FilterType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -47,6 +49,9 @@ class FrontendAjaxController extends Controller
 
         if (null === ($response = $filter->handleForm())) {
             throw new HandleFormException('Unable to handle form for filter with id '.$id.'.');
+        }
+        if ($request->get($filter->getFilter()['name']) && isset($request->get($filter->getFilter()['name'])[FilterType::FILTER_REFERRER_NAME])) {
+            Environment::set('request', $request->get($filter->getFilter()['name'])[FilterType::FILTER_REFERRER_NAME]);
         }
 
         $index = new FrontendIndex(); // initialize BE_USER_LOGGED_IN or FE_USER_LOGGED_IN
