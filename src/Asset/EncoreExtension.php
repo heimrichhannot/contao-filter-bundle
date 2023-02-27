d694b3b83337a530b70c2ec7ebf794f6d069262c
Thomas <koertho@users.noreply.github.com>
2023-02-27 14:09:42 +0100

    Add encore contracts support (#28)

---
+++ src/Asset/EncoreExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Copyright (c) 2023 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\FilterBundle\Asset;
+
+use HeimrichHannot\EncoreContracts\EncoreEntry;
+use HeimrichHannot\EncoreContracts\EncoreExtensionInterface;
+use HeimrichHannot\FilterBundle\HeimrichHannotContaoFilterBundle;
+
+class EncoreExtension implements EncoreExtensionInterface
+{
+    public function getBundle(): string
+    {
+        return HeimrichHannotContaoFilterBundle::class;
+    }
+
+    public function getEntries(): array
+    {
+        return [
+            EncoreEntry::create('contao-filter-bundle', 'src/Resources/assets/js/contao-filter-bundle.js')
+                ->addJsEntryToRemoveFromGlobals('contao-filter-bundle'),
+        ];
+    }
+}
