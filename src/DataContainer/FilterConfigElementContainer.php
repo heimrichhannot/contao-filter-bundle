f7727829d706f4a94055b59726e20bad3b48215b
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-02-27 13:59:17 +0100

    small fix

--- src/DataContainer/FilterConfigElementContainer.php
+++ src/DataContainer/FilterConfigElementContainer.php
@@ -83,7 +83,7 @@ class FilterConfigElementContainer
         $enabled = $class::isEnabledForCurrentContext($context);
 
         return '<div class="tl_content_left">'
-            .($enabled ? '' : Image::getHtml('error.svg', $this->translator->trans('huh.filter.warning.not_supported_in_current_context'), 'style="padding-right: 3px;" title="'.$this->translator->trans('huh.filter.warning.not_supported_in_current_content').'"'))
+            .($enabled ? '' : Image::getHtml('error.svg', '', 'style="padding-right: 3px;" title="'.$this->translator->trans('huh.filter.warning.not_supported_in_current_content').'"'))
             .($row['title'] ?: $row['id']).' <span style="color:#b3b3b3; padding-left:3px">['.($GLOBALS['TL_LANG']['tl_filter_config_element']['reference']['type'][$row['type']] ?: $row['type']).($row['isInitial'] ? ' – Initial' : '').']</span></div>';
     }
 
